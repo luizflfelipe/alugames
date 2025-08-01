d9b69773074c8b5b67d3be39398bb4760e86ed5d
Luiz Felipe <luisffelipe20@gmail.com>
2025-08-01 18:25:03 -0300

    Project Name: Alugames Description: Adicionando a funcionalidade de alterar o texto do botão de "Alugar" para "Devolver" e vice-versa, dependendo do status do jogo. Adicionar mais interações com o botão, como adicionar um evento de clique para chamar a função `alterarBotao` quando o botão for clicado.

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function alterarStatus(id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')){
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        confirm('Tem certeza que deseja devolver este jogo?');
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+        confirm('Tem certeza que deseja alugar este jogo?');
+    }quatidadeDeJogosAlugados();
+}
+function quatidadeDeJogosAlugados() {
+    let jogosALugados = document.querySelectorAll('.dashboard__item__img--rented');
+    let quantidade = jogosALugados.length;
+    console.log(`Quantidade de jogos alugados: ${quantidade}`);
+    document.querySelector('h1').textContent = `Jogos Alugados: ${quantidade}`;
+    if (quantidade == 0){
+        document.querySelector('h1').textContent = 'Aluguel de boardgames';
+    }
+}
